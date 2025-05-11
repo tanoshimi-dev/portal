08eff0de65725806d1d561764fdb7c80ba4a77aa
tanoshimi-dev <keijimitaki@gmail.com>
2025-05-11 09:59:01 +0900

    自動再生の雛形追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,32 @@
+import React, { useEffect } from 'react';
 export const Footer = () => {
-    return (
-      <div className="footer_links">
-        ©2024 Tanoshimi.dev All Rights Reserved
-      </div>
-    );
+
+  const playSound = () => {
+    const audio = document.getElementById('myAudio');
+    audio.play();
+        // Scroll the page
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+
+        // Click the links
+        const link2 = document.getElementById('link_skills');
+        const link3 = document.getElementById('link_contact');
+
+        if (link2) link2.click();
+       // if (link3) link3.click();
   };
+
+  const stopSound = () => {
+    const audio = document.getElementById('myAudio');
+    audio.pause();
+    audio.currentTime = 0; // Reset the audio to the beginning
+  };  
+  
+  return (
+    <div className="footer_links">
+      <button onClick={playSound}>Play</button>
+      <button onClick={stopSound}>Stop</button>      
+      ©2025 Tanoshimi.dev All Rights Reserved
+    </div>
+  );
+
+};
